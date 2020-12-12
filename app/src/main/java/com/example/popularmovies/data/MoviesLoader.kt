4a5b82a73a957bf9692341517966b6ffe8b1978e
Rafael Balthazar <rafael@tauto.com.br>
2020-12-12 10:13:01 -0300

    Changed database MoviesViewModel to Kotlin coroutines

--- app/src/main/java/com/example/popularmovies/data/MoviesLoader.kt
+++ app/src/main/java/com/example/popularmovies/data/MoviesLoader.kt
@@ -1,7 +1,6 @@
 package com.example.popularmovies.data
 
 import android.app.Application
-import android.os.AsyncTask
 import com.example.popularmovies.model.Movie
 import com.example.popularmovies.util.MoviesFetcher
 import com.example.popularmovies.util.Utilities
@@ -10,12 +9,11 @@ import java.util.*
 class MoviesLoader(
         private val application: Application,
         private var forceFetch: Boolean,
-        private val liveDataHolder: MoviesLiveData
-) : AsyncTask<Void, Void, List<Movie>>() {
+) {
 
     private val database = MoviesDbHelper.getDatabase(application)
 
-    override fun doInBackground(vararg params: Void?): List<Movie> {
+    fun doLoad(): List<Movie> {
         val moviesOrder = Utilities.getPreferredSelection(application)
         if (needsFetching(moviesOrder)) {
             val moviesFetcher = MoviesFetcher(application)
@@ -28,10 +26,6 @@ class MoviesLoader(
         val movieLists = movieListDao.findByListId(list.id)
         val movieIds = movieLists.map { it.movieId }
         return movieDao.loadByIds(movieIds.toLongArray())
-    }
-
-    override fun onPostExecute(result: List<Movie>) {
-        liveDataHolder.setValueExternally(result)
     }
 
     private fun needsFetching(moviesOrder: String): Boolean {
--- app/src/main/java/com/example/popularmovies/data/MoviesViewModel.kt
+++ app/src/main/java/com/example/popularmovies/data/MoviesViewModel.kt
@@ -1,12 +1,19 @@
 package com.example.popularmovies.data
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.*
+import com.example.popularmovies.model.Movie
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MoviesViewModel(application: Application): AndroidViewModel(application) {
-    val data = MoviesLiveData(application)
+    val data: MutableLiveData<List<Movie>> = MutableLiveData()
 
     fun loadData(forceFetch: Boolean) {
-        data.load(forceFetch)
+        viewModelScope.launch(Dispatchers.IO) {
+            val loader = MoviesLoader(getApplication(), forceFetch)
+            val movies = loader.doLoad()
+            data.postValue(movies);
+        }
     }
 }
